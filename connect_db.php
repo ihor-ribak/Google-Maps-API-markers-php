4230858a7eaa38e10b1e57287c29b4a43f551d0a
Ihor Rybak <ihor2014.19@gmail.com>
2021-02-19 15:15:53 +0200

    API-markers

--- connect_db.php
+++ connect_db.php
@@ -4,7 +4,7 @@ function pdo_connect_mysql()
 {
     $DATABASE_HOST = 'localhost';
     $DATABASE_USER = 'igor';
-    $DATABASE_PASS = 'Password3';
+    $DATABASE_PASS = 'xxxxxxxxxxxx';
     $DATABASE_NAME = 'google_api';
     try {
         return new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME . ';charset=utf8', $DATABASE_USER, $DATABASE_PASS);
